d4c2abc23eabe2e0cb41badd1958736b2337dbf9
jitain1 <singhjitainj@gmail.com>
2023-02-16 20:28:47 +0530

    Added: JDBC EmployeWage Code

---
+++ src/main/java/com/bridgelabz/Day34/RetrievingDataFromDb.java
@@ -0,0 +1,41 @@
+package com.bridgelabz.Day34;
+import com.mysql.cj.protocol.Resultset;
+
+import java.sql.*;
+import java.util.*;
+
+
+public class RetrievingDataFromDb {
+	
+	
+
+	    private static List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+
+	    public static void main(String[] args) {
+
+
+	        String query = "select * from employee_payroll";
+	        Connection connection = null;
+	        Statement statement = null;
+	        try {
+	            Class.forName("com.mysql.cj.jdbc.Driver");
+	            System.out.println("Driver class loaded and registered");
+	            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_service", "root", "Jitain@88");
+	            System.out.println("Connection established with db server");
+	            statement = connection.createStatement();
+	            System.out.println("platform created/Statement Object Created");
+	            ResultSet resultset=statement.executeQuery(query);
+	            System.out.println("Data selected from DB");
+	            while(resultset.next()){
+	                int id=resultset.getInt("id");
+	                String name=resultset.getString("name");
+	                double salary=resultset.getDouble("salary");
+	                employeePayrollList.add(new EmployeePayrollData(id,name,salary));
+	            }
+	            System.out.println(employeePayrollList);
+
+	        } catch (ClassNotFoundException | SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	}
